7e4bab59f788a61e309eafab750119804a27a85b
Abhimanyu Patil <abhipatil9798@gmail.com>
2024-06-21 23:46:25 +0530

    Assignment Completed

---
+++ Assign3_Que4/src/com/sunbeam/singlyLinear/Main.java
@@ -0,0 +1,23 @@
+package com.sunbeam.singlyLinear;
+
+import java.util.Scanner;
+
+
+public class Main {
+   
+	public static void SortedInsertion(List l1) {
+		
+	}
+ public static void main(String[] args) {
+        
+	     List l1 = new List();
+	     l1.sortedInsetion(50);
+	     l1.sortedInsetion(40);
+	     l1.sortedInsetion(40);
+	     l1.sortedInsetion(30);
+	     l1.sortedInsetion(20);
+	     l1.sortedInsetion(10);
+	     
+	     l1.display();
+	    }
+	}
